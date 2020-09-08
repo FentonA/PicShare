6118542c806f990209100bd597580e6aec0afb93
Alf Fenton <Fentonalf@gmail.com>
2020-09-09 02:49:51 +0700

    real commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,100 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Particles from "react-tsparticles";
 
 const App = () =>(
-  <div>
-    <div className='body'>
+  <div className="App">
+<Particles
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "#0d47a1",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    <h1 className="title">PICSHARE</h1>
+    <h2 className="title">Please upload an image below :)</h2>
+    <form action="http://127.0.0.1:5000/upload" method="POST" enctype="multipart/form-data">
+      <label for="myfile">Select a file:</label>
+      <input type="file" id="pic" name="pic"/>
+      <input type="submit"/>
+    </form>
 
-    </div>
   </div>
-)
+);
 
 export default App;
---
+++ frontend/src/pages/gallery.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './gallery.styles.scss'
+
+const Gallery =() =>(
+    <h1>This is the Gallery</h1>
+)
+
+export default Gallery
